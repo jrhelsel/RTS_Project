0098a661c1f6c88db399fb41596a72b7fac08e6f
jrhelsel <jasenrhelsel@gmail.com>
2023-10-01 17:24:00 -0500

    multiplayer stable, camera changes

--- scripts/rts_camera.gd
+++ scripts/rts_camera.gd
@@ -2,27 +2,25 @@ extends Node3D
 
 @export var pan_speed = 30.0
 
-var zooming = false
-
 func _ready():
 	pass
 
 
 
 func _process(delta):
-	if $Camera3D.current:
+	if $Camera3D.current or $Camera3D/ToRTSTransitionCamera.current:
 		var viewport_size = get_viewport().size
 		var mouse_pos = get_viewport().get_mouse_position()
 		var confined = Input.mouse_mode == Input.MOUSE_MODE_CONFINED
 		
 		if Input.is_action_pressed("pan_left") or (mouse_pos.x < 10 and confined):
-			position.x -= pan_speed * delta
+			global_position.x -= pan_speed * delta
 		elif Input.is_action_pressed("pan_right") or (mouse_pos.x > viewport_size.x - 10 and confined):
-			position.x += pan_speed * delta
+			global_position.x += pan_speed * delta
 		if Input.is_action_pressed("pan_up") or (mouse_pos.y < 10 and confined):
-			position.z -= pan_speed * delta
+			global_position.z -= pan_speed * delta
 		elif Input.is_action_pressed("pan_down") or (mouse_pos.y > viewport_size.y - 10 and confined):
-			position.z += pan_speed * delta
+			global_position.z += pan_speed * delta
 		
 		
 		#---------
