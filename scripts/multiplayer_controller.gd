dfdb526dc78af202a69c82d15f135d5358fc280c
jrhelsel <jasenrhelsel@gmail.com>
2023-10-04 20:22:17 -0500

    internet trial

--- scripts/multiplayer_controller.gd
+++ scripts/multiplayer_controller.gd
@@ -73,6 +73,9 @@ func _on_host_button_down():
 
 
 func _on_join_button_down():
+	if !($Address.text == ""):
+		address = $Address.text
+		
 	peer = ENetMultiplayerPeer.new()
 	peer.create_client(address, port)
 	peer.get_host().compress(compression)
